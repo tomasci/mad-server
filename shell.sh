7804ae5b89b46ead2ce48a2f3d1705f97e58d634
tomasci <zivgta@gmail.com>
2024-09-12 00:56:20 +0300

    Updated bruno docs. Added basic client files (vite.js react app)

--- shell.sh
+++ shell.sh
@@ -15,6 +15,9 @@ alias mad:build="bash ./scripts/build.sh"
 # run build
 alias mad:run="bash ./scripts/run.sh"
 
+# run client in dev mode
+alias mad:client:dev="npm --prefix _client run dev"
+
 # display amount of written go-lines in project
 alias mad:stats="bash ./scripts/stats.sh"
 
@@ -34,5 +37,29 @@ alias mad:m:c="mad:migrate:create"
 
 # add any other custom settings or configurations
 #export MY_VAR="some_value"
+
+mad:help() {
+  echo "\nHelp"
+  echo ""
+  echo "* Backend"
+  printf "%-20s %s\n" "mad:dev" "run server in dev mode"
+  printf "%-20s %s\n" "mad:build" "build server"
+  printf "%-20s %s\n" "mad:run" "run build"
+  echo ""
+  echo "* Migrations"
+  printf "%-20s %s\n" "mad:migrate:up" "apply new migrations"
+  printf "%-20s %s\n" "mad:m:u" ""
+  printf "%-20s %s\n" "mad:migrate:down" "rollback (one by one)"
+  printf "%-20s %s\n" "mad:m:d" ""
+  printf "%-20s %s\n" "mad:migrate:create" "create new .sql migration file"
+  printf "%-20s %s\n" "mad:m:c" ""
+  echo ""
+  echo "* Frontend"
+  printf "%-20s %s\n" "mad:client:dev" "run client in dev mode"
+  echo ""
+  echo "* Other"
+  printf "%-20s %s\n" "mad:stats" "display amount of written [go, tsx]-lines in project"
+  echo ""
+}
 
 echo "MAD Project Environment Loaded"
